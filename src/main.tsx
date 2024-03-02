7fa5f338162c499acbfd11198f71e9d1818842f9
notmyfault <fallenangelhd06@gmail.com>
2024-03-02 01:57:27 +0000

    feat: Routes to other pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './global.css'
+import { Home } from './pages/Home.tsx';
+import { Weapons } from './pages/Weapons.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='weapons' element={<Weapons/>}/>
+        {/* <Route path='stratagems' element={<Home/>} />
+        <Route path='loadouts' element={<Home/>} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
