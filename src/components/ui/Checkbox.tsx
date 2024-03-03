2217e69808dac711978864ffac47eccbda3599a9
notmyfault <fallenangelhd06@gmail.com>
2024-03-03 01:16:09 +0000

    Visual Change

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -16,10 +16,10 @@ export function Checkbox(props:props){
     return(
         <div className="flex gap-1.5 items-center hover:cursor-pointer" onClick={hToggle}>
             { toggle ?
-            <div className="bg-neutral-800 w-3 h-3 rounded">
+            <div className="bg-red-700 w-3 h-3 rounded">
             </div> : 
-            <div className="relative bg-neutral-950 w-3 h-3 rounded">
-                <Check className='absolute text-white w-4 h-4 -top-[0.15rem]'/>
+            <div className="relative bg-black w-3 h-3 rounded">
+                <Check className='absolute text-white w-5 h-5 -top-[0.30rem] -left-[0.1rem]'/>
             </div>
             }
             <div className='mt-1'>
