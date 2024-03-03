a8833dc6e8ae4b38ce34f4a5383358923de21b23
notmyfault <fallenangelhd06@gmail.com>
2024-03-03 01:15:56 +0000

    Tanstack Integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import './global.css'
 import { Home } from './pages/Home.tsx';
 import { Weapons } from './pages/Weapons.tsx';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home/>}/>
-        <Route path='weapons' element={<Weapons/>}/>
-        {/* <Route path='stratagems' element={<Home/>} />
-        <Route path='loadouts' element={<Home/>} /> */}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}/>
+            <Route path='weapons' element={<Weapons/>}/>
+            {/* <Route path='stratagems' element={<Home/>} />
+            <Route path='loadouts' element={<Home/>} /> */}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
