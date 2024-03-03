a9cc5a85089aed04051a4040358fd442a22d726c
notmyfault <fallenangelhd06@gmail.com>
2024-03-03 01:16:17 +0000

    -

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,12 @@ import { Hero } from "../components/Hero";
 import { Information } from "../components/Information";
 import { Navbar } from "../components/Navbar";
 
-export function Home(){
-    return(
-        <div>
-            <Navbar page={0}/>
-            <Hero/>
-            <Information/>
-        </div>
-    );
+export function Home() {
+  return (
+    <div>
+      <Navbar page={0} />
+      <Hero />
+      <Information />
+    </div>
+  );
 }
